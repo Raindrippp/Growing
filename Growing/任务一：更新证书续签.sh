75a111c5e04e9c769af5691e8d9b1ee8d690adfd
牧守流光 <87850955+Raindrippp@users.noreply.github.com>
2021-10-02 07:30:38 +0800

    Update 任务一：更新证书续签.sh

--- Growing/任务一：更新证书续签.sh
+++ Growing/任务一：更新证书续签.sh
@@ -75,3 +75,48 @@ else
                 echo "无需更新"
                 #最小有效期满足要求，无需更新。
 fi
+
+
+第四版
+#! /usr/bin/bash
+##########################################
+# Function: 延长组件证书有效期           #
+# Author: DingHang                       #
+# CreateTime: 2021/9/27                  #
+##########################################
+
+set -o nounset
+set -o errexit
+#Sed -i 's/\r//' clear_sshd_process.sh  format the script.
+
+# Export the LANG environment.
+vOsType=$(uname | tr '[a-z]' '[A-Z]')
+if [ "${vOsType}" = "AIX" -o "${vOsType}" = "LINUX" ];then
+	export LANG=es_US.utf8
+fi
+
+T=$1
+MINI_TIME1=$(kubeadm alpha certs check-expiration | grep  -A9 'admin' | awk '{print $7}' | grep 'd' | sed 's/.$'// | awk 'NR==1{min=$1;next}{min=min<$1?min:$1}END{print min}')
+MINI_TIME2=$(kubeadm alpha certs check-expiration | grep  -A9 'admin' | awk '{print $7}' | grep 'y' | sed 's/.$'// | awk 'NR==1{min=$1;next}{min=min<$1?min:$1}END{print min}')
+#输入最低有效期天数
+
+if [ ! $MINI_TIME1 ]; then
+  MINI_TIME=$(expr $MINI_TIME2 \* 365 )
+else
+  MINI_TIME=$MINI_TIME1
+fi
+
+if [ $MINI_TIME -lt $T ]
+        then
+                kubeadm alpha certs renew all
+                #最小有效期低于要求时更新组件有效期
+                if [ $? -eq 0 ]
+                #判断上一步更新是否成功
+        then
+                echo "更新成功"
+        #更新完成后提示更新成功
+fi
+else
+                echo "无需更新"
+                #最小有效期满足要求，无需更新。
+fi
