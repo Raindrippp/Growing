bce0c501b785aa3daca12d3a57683ee630ff7e2d
牧守流光 <87850955+Raindrippp@users.noreply.github.com>
2021-12-06 14:03:47 +0800

    Update DockerPrune.py

--- 任务三：镜像清理/DockerPrune.py
+++ 任务三：镜像清理/DockerPrune.py
@@ -1,5 +1,36 @@
+import os
+from logging.handlers import TimedRotatingFileHandler
+
 import docker
 import logging
+
+path = '/root'
+if not os.path.exists('log'):
+    os.makedirs('log')
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+LOG_HANDLER = TimedRotatingFileHandler(filename='/root/log/myProgramLog.txt', when='midnight',
+                                       interval=1, backupCount=7)
+FORMAT = '%(asctime)s %(levelname)s %(message)s'
+DATA_FORMAT = '%Y-%m-%d %H:%M:%S'
+FOR_MATTER = logging.Formatter(FORMAT, DATA_FORMAT)
+LOG_HANDLER.setFormatter(FOR_MATTER)
+LOGGER.addHandler(LOG_HANDLER)
+
+
+def info(msg):
+    LOGGER.info(msg)
+    return
+
+
+def warning(msg):
+    LOGGER.warning(msg)
+    return
+
+
+def error(msg):
+    LOGGER.error(msg)
+    return
 
 
 class Docker_Prune:
@@ -12,10 +43,10 @@ class Docker_Prune:
         try:
             list0 = self.client.images.prune(filters={'dangling': False})
         except Exception as e:
-            logging.error(f'清理无用镜像失败,错误信息{e}')
+            error(f'清理无用镜像失败,错误信息{e}')
             return None
         else:
-            logging.info(f"成功清理镜像:{list0['ImagesDeleted']}")
+            info(f"成功清理镜像:{list0['ImagesDeleted']}")
         finally:
             pass
 
